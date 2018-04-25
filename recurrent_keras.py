aecdf08f0bb60c5f37f70dfc4c1173122864b674
Kai <kai0levy@gmail.com>
2018-04-24 23:54:01 -0400

    Update with random generation and starting string

--- recurrent_keras.py
+++ recurrent_keras.py
@@ -21,6 +21,8 @@ ap.add_argument('-generate_length', type=int, default=500)
 ap.add_argument('-nb_epoch', type=int, default=20)
 ap.add_argument('-mode', default='train')
 ap.add_argument('-weights', default='')
+ap.add_argument('-start', default='')
+ap.add_argument('-randgen', default='')
 args = vars(ap.parse_args())
 
 DATA_DIR = args['data_dir']
@@ -29,11 +31,14 @@ HIDDEN_DIM = args['hidden_dim']
 SEQ_LENGTH = args['seq_length']
 WEIGHTS = args['weights']
 
+start = args['start']
+randgen = args['randgen']
+
 GENERATE_LENGTH = args['generate_length']
 LAYER_NUM = args['layer_num']
 
 # Creating training data
-X, y, VOCAB_SIZE, ix_to_char = load_data(DATA_DIR, SEQ_LENGTH)
+X, y, VOCAB_SIZE, ix_to_char, char_to_ix = load_data(DATA_DIR, SEQ_LENGTH)
 
 # Creating and compiling the Network
 model = Sequential()
@@ -45,7 +50,8 @@ model.add(Activation('softmax'))
 model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
 
 # Generate some sample before training to know how bad it is!
-generate_text(model, args['generate_length'], VOCAB_SIZE, ix_to_char)
+#print("\n\nGenerating untrained text...\n\n")
+#generate_text(model, args['generate_length'], VOCAB_SIZE, ix_to_char, -1, 0)
 
 if not WEIGHTS == '':
   model.load_weights(WEIGHTS)
@@ -59,7 +65,7 @@ if args['mode'] == 'train' or WEIGHTS == '':
     print('\n\nEpoch: {}\n'.format(nb_epoch))
     model.fit(X, y, batch_size=BATCH_SIZE, verbose=1, nb_epoch=1)
     nb_epoch += 1
-    generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char)
+    generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char, -1, 0)
     if nb_epoch % 10 == 0:
       model.save_weights('checkpoint_layer_{}_hidden_{}_epoch_{}.hdf5'.format(LAYER_NUM, HIDDEN_DIM, nb_epoch))
 
@@ -67,7 +73,30 @@ if args['mode'] == 'train' or WEIGHTS == '':
 elif WEIGHTS == '':
   # Loading the trained weights
   model.load_weights(WEIGHTS)
-  generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char)
+  generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char, -1, 0)
   print('\n\n')
+# elif not start == '':
+#   if not randgen == '':
+#     print("\n\nGenerating random text with start..\n\n")
+#     start_ix = [char_to_ix[s] for s in start]
+#     generate
+#   else:
+#     print("\n\nGenerating text with start...\n\n")
+#     start_ix = [char_to_ix[s] for s in start]
+#     generate_with_start(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char, start_ix)
+# elif not randgen == '':
+#   print("\n\nGenerating random text..\n\n")
+#   generate_random(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char)
 else:
-  print('\n\nNothing to do!')
+  print("\n\nGenerating trained text...\n\n")
+  if not start == '':
+    print("with start...")
+    start_ix = [char_to_ix[s] for s in start]
+  else:
+    start_ix = -1
+  if not randgen == '':
+    print("randomly..")
+    randgen = 1
+  else:
+    randgen = 0
+  generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char, start_ix, randgen)
